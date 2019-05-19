9806788ebb362dfa791b411f085ebadc2ae72f11
keyanbagheri <keyanbagheri@gmail.com>
2019-05-19 12:53:08 +0200

    updating link to medium for briefly

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
         <FullLayout path='/selected-work/:id' component={Project}/>
         <BlackLayout path='/photos' component={PhotoScroller} />
         <Route exact path='/teamweek' component={() => { window.location = 'https://projects.invisionapp.com/prototype/teamweek-mobile-cjuy1ccny0093gw015u6u6a8c'; return null;} }/>
-        <Route exact path='/briefly' component={() => { window.location = 'https://invis.io/P3RUPQ29V6J'; return null;} }/>
+        <Route exact path='/briefly' component={() => { window.location = 'https://medium.com/@morrison_key/briefly-get-briefed-on-the-fly-754c92ac1e9?source=friends_link&sk=092cf7a01d61f1b2cdbb6803ee4d7316'; return null;} }/>
         <Route exact path='/soundcloud' component={() => { window.location = 'https://invis.io/SBRHYFEFC7M'; return null;} }/>
         <Route exact path='/reincarnage' component={() => { window.location = 'http://reincarnage.herokuapp.com'; return null;} }/>
         <Route exact path='/leadfeeder' component={() => { window.location = 'https://leadfeeder.com'; return null;} }/>
--- src/components/menu/menu-view.js
+++ src/components/menu/menu-view.js
@@ -10,8 +10,8 @@ const Menu = (props) => {
       <ul>
         <li><Link to='/' className="menu-link">Selected Works</Link></li>
         <li><Link to='/about' className="menu-link">About</Link></li>
-        <li><Link to='/photos' className="menu-link white">Photography</Link></li>
-        <li><Link to='/404' className="menu-link white">404</Link></li>
+        <li><Link to='/photos' className="menu-link white-link">Photography</Link></li>
+        <li><Link to='/404' className="menu-link white-link">404</Link></li>
       </ul>
       <div className="black-half"></div>
     </nav>
