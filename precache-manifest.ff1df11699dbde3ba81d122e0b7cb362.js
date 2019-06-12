5b280f0b8e8e3d82af8641ced89e5a60e9966292
keyanbagheri <keyanbagheri@gmail.com>
2019-06-12 15:41:11 +0200

    Updates

---
+++ precache-manifest.ff1df11699dbde3ba81d122e0b7cb362.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0b05f0de67ace83e3d814f2682ff48",
+    "url": "/morrisonkey.com/index.html"
+  },
+  {
+    "revision": "34e6ce1af7b9666eea7e",
+    "url": "/morrisonkey.com/static/css/main.e2ce8f78.chunk.css"
+  },
+  {
+    "revision": "90ebc3cdfc0d8229cc59",
+    "url": "/morrisonkey.com/static/js/2.c6bc9356.chunk.js"
+  },
+  {
+    "revision": "34e6ce1af7b9666eea7e",
+    "url": "/morrisonkey.com/static/js/main.37b5f69e.chunk.js"
+  },
+  {
+    "revision": "931076e3d4e13963f2b8",
+    "url": "/morrisonkey.com/static/js/runtime~main.9f326637.js"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/morrisonkey.com/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/morrisonkey.com/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "af458891a0f565ab1ed5f104f4b09617",
+    "url": "/morrisonkey.com/static/media/bird-sprite.af458891.png"
+  },
+  {
+    "revision": "657927477aa2c216bf37225b69401203",
+    "url": "/morrisonkey.com/static/media/briefly.65792747.png"
+  },
+  {
+    "revision": "1ee5ec5e28c19f94ce237239f3aada49",
+    "url": "/morrisonkey.com/static/media/cv.1ee5ec5e.jpg"
+  },
+  {
+    "revision": "73fdebc6a4e9141d61ca36d7f086a5d2",
+    "url": "/morrisonkey.com/static/media/dribbble-logo.73fdebc6.svg"
+  },
+  {
+    "revision": "5d75b2c4ecaf0bd27d31de227cc5d09d",
+    "url": "/morrisonkey.com/static/media/filterbuds.5d75b2c4.png"
+  },
+  {
+    "revision": "77d969975131d9774cbc16d71024eef6",
+    "url": "/morrisonkey.com/static/media/github-logo.77d96997.svg"
+  },
+  {
+    "revision": "8b12a9350599a504362d5dd0cea348e1",
+    "url": "/morrisonkey.com/static/media/img1.8b12a935.jpg"
+  },
+  {
+    "revision": "970913d90e1018768379a9e698bd5238",
+    "url": "/morrisonkey.com/static/media/img13.970913d9.jpg"
+  },
+  {
+    "revision": "246c087baf9ff01901f9202070decd1e",
+    "url": "/morrisonkey.com/static/media/img14.246c087b.jpg"
+  },
+  {
+    "revision": "f240f06e065a0adf10b255d2dde305f2",
+    "url": "/morrisonkey.com/static/media/img15.f240f06e.jpg"
+  },
+  {
+    "revision": "38324550ba103c498d4ebf3764ffbf76",
+    "url": "/morrisonkey.com/static/media/img16.38324550.jpg"
+  },
+  {
+    "revision": "544306e37727726253e8da3b0a3304cb",
+    "url": "/morrisonkey.com/static/media/img2.544306e3.jpg"
+  },
+  {
+    "revision": "1ceb619f0150894fc4e1a5722279d993",
+    "url": "/morrisonkey.com/static/media/img20.1ceb619f.jpg"
+  },
+  {
+    "revision": "32f4dc7b1a03d37dc16c289100c4e3a5",
+    "url": "/morrisonkey.com/static/media/img24.32f4dc7b.jpg"
+  },
+  {
+    "revision": "abd395270dadf434fa957f2cdd617536",
+    "url": "/morrisonkey.com/static/media/img3.abd39527.jpg"
+  },
+  {
+    "revision": "d0b5c0f2d78141b21454c823b126ae3f",
+    "url": "/morrisonkey.com/static/media/img4.d0b5c0f2.jpg"
+  },
+  {
+    "revision": "b3d9e65384c93f4dd1c5262e95292422",
+    "url": "/morrisonkey.com/static/media/img5.b3d9e653.jpg"
+  },
+  {
+    "revision": "ebbb70ac697cd36cc00316db8b86c442",
+    "url": "/morrisonkey.com/static/media/img7.ebbb70ac.jpg"
+  },
+  {
+    "revision": "f23df8b8fd8b14f5d2e8671df554cf13",
+    "url": "/morrisonkey.com/static/media/img9.f23df8b8.jpg"
+  },
+  {
+    "revision": "7b94e2c2203563979f69393babe0af14",
+    "url": "/morrisonkey.com/static/media/instagram-logo.7b94e2c2.svg"
+  },
+  {
+    "revision": "255aef63b58333f7d1831f64c43910a4",
+    "url": "/morrisonkey.com/static/media/leadfeeder.255aef63.png"
+  },
+  {
+    "revision": "ea4e1c08962ae7cf4b36f020ecdefab8",
+    "url": "/morrisonkey.com/static/media/linkedin-logo.ea4e1c08.svg"
+  },
+  {
+    "revision": "bb9c7954dbcda68f9809fe62b570a83c",
+    "url": "/morrisonkey.com/static/media/me-black.bb9c7954.jpg"
+  },
+  {
+    "revision": "1e11238281d49dd4e0c2cb3700d849b9",
+    "url": "/morrisonkey.com/static/media/medium-logo.1e112382.svg"
+  },
+  {
+    "revision": "0d05a95e81ef1f79f4a8c497ccc1c26b",
+    "url": "/morrisonkey.com/static/media/nsrinvest.0d05a95e.png"
+  },
+  {
+    "revision": "a0728d182a01ca7c63f815b7c79bd51a",
+    "url": "/morrisonkey.com/static/media/reincarnage.a0728d18.png"
+  },
+  {
+    "revision": "8a44972e2baabe6f9c3c6a375bb75e40",
+    "url": "/morrisonkey.com/static/media/soundcloud.8a44972e.png"
+  },
+  {
+    "revision": "3acba61121a24c9705b57bca89dd69bb",
+    "url": "/morrisonkey.com/static/media/teamweek.3acba611.gif"
+  }
+]);
